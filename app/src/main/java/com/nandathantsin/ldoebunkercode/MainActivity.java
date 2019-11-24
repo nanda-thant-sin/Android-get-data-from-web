fca26446d43bf5135ffe5f96c6d10549e061e766
nanda-thant-sin <jacepot@gmail.com>
2019-11-24 13:04:25 +0630

    Add dark theme

--- app/src/main/java/com/nandathantsin/ldoebunkercode/MainActivity.java
+++ app/src/main/java/com/nandathantsin/ldoebunkercode/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -24,17 +26,26 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
     private String alfa_link = "https://last-day-on-earth-survival.fandom.com/wiki/Bunker_Alfa";
+    private int[] daysInMonth = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
     private int day;
     private int month;
+    private int year;
     TextView code;
     TextView heading;
     TextView no_connection;
+    TextView otherCode;
     Button btn_retry;
+    Button btn_wrong_code;
     ProgressDialog progressDialog;
-
+    LinearLayout twoMoreCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,8 +53,11 @@ public class MainActivity extends AppCompatActivity {
         Date today = new Date(); // Fri Jun 17 14:54:28 PDT 2016
         Calendar cal = Calendar.getInstance();
         cal.setTime(today);
+        twoMoreCode = findViewById(R.id.twoMoreCode);
+        otherCode = findViewById(R.id.otherCode);
         day = cal.get(Calendar.DAY_OF_MONTH); // 17
         month = cal.get(Calendar.MONTH); // 5
+        year = cal.get(Calendar.YEAR);
         code = findViewById(R.id.code);
         heading = findViewById(R.id.heading);
         no_connection = findViewById(R.id.no_connection);
@@ -54,7 +68,38 @@ public class MainActivity extends AppCompatActivity {
                 new InternetCheck().execute();
             }
         });
-        new InternetCheck().execute();
+        btn_wrong_code = findViewById(R.id.wrong_code);
+        btn_wrong_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new InternetCheck().execute();
+            }
+        });
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        String str = year+""+month;
+        String savedString = prefs.getString("codes", "");
+        String[] codes = savedString.split(",");
+        if(codes[0].equals(str)){
+            code.setText(codes[day]);
+            String yesterday;
+            String tomorrow;
+            if(day==1){
+                yesterday = prefs.getString("yesterday","");
+                tomorrow = codes[day+1];
+            }
+            else if(day==daysInMonth[month]){
+                yesterday = codes[day-1];
+                tomorrow = prefs.getString("tomorrow","");
+            }
+            else{
+                yesterday = codes[day-1];
+                tomorrow = codes[day+1];
+            }
+            otherCode.setText(yesterday+"\n"+tomorrow);
+        }
+        else{
+            new InternetCheck().execute();
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,12 +155,16 @@ public class MainActivity extends AppCompatActivity {
             if (internet) {
                 heading.setVisibility(View.VISIBLE);
                 code.setVisibility(View.VISIBLE);
+                twoMoreCode.setVisibility(View.VISIBLE);
+                btn_wrong_code.setVisibility(View.VISIBLE);
                 no_connection.setVisibility(View.GONE);
                 btn_retry.setVisibility(View.GONE);
                 new Alfa().execute(alfa_link);
             } else {
                 heading.setVisibility(View.GONE);
                 code.setVisibility(View.GONE);
+                twoMoreCode.setVisibility(View.GONE);
+                btn_wrong_code.setVisibility(View.GONE);
                 no_connection.setVisibility(View.VISIBLE);
                 btn_retry.setVisibility(View.VISIBLE);
             }
@@ -141,13 +190,36 @@ public class MainActivity extends AppCompatActivity {
                 Document document = Jsoup.connect(strings[0]).get();
                 Elements table = document.getElementsByClass("article-table");
                 Elements tr = table.get(1).select("tr");
-                Elements td = tr.get(day).select("td");
-                for (Element cell : td) {
-                    arrayList.add(cell.text());
-                    //System.out.println(cell.text());
+                String yesterday = "Not found";
+                String tomorrow = "Not found";
+                int count = 0;
+                int daysInLastMonth = daysInMonth[month-1];
+                if(month==1 && new GregorianCalendar().isLeapYear(year)){
+                    daysInLastMonth++;
                 }
+                for(Iterator<Element> iter = tr.iterator(); iter.hasNext(); ) {
+                    Elements td = iter.next().getElementsByTag("td");
+                    if(td.size()!=0)
+                        arrayList.add(td.get(month).text());
+                    if(month!=11 && count==1){
+                        tomorrow = td.get(month+1).text();
+                    }
+                    else if(month!=0 && count==daysInLastMonth){
+                        yesterday = td.get(month-1).text();
+                    }
+                    count++;
+                }
+                SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+                prefs.edit().putString("yesterday", yesterday).commit();
+                prefs.edit().putString("tomorrow", tomorrow).commit();
+//                Elements td = tr.get(day).select("td");
+//                for (Element cell : td) {
+//                    arrayList.add(cell.text());
+//                    //System.out.println(cell.text());
+//                }
             } catch (IOException e) {
                 e.printStackTrace();
+
                 return null;
             }
 
@@ -160,11 +232,37 @@ public class MainActivity extends AppCompatActivity {
             if(arrayList==null){
                 heading.setVisibility(View.GONE);
                 code.setVisibility(View.GONE);
+                twoMoreCode.setVisibility(View.GONE);
+                btn_wrong_code.setVisibility(View.GONE);
                 no_connection.setVisibility(View.VISIBLE);
                 btn_retry.setVisibility(View.VISIBLE);
             }
             else {
-                code.setText(arrayList.get(month));
+                //code.setText(arrayList.get(month));
+                SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+                StringBuilder str = new StringBuilder(year+""+month+",");
+                for (int i = 0; i < arrayList.size(); i++) {
+                    str.append(arrayList.get(i)).append(",");
+                }
+                prefs.edit().putString("codes", str.toString()).commit();
+                String savedString = prefs.getString("codes", "");
+                String[] codes = savedString.split(",");
+                code.setText(arrayList.get(day));
+                String yesterday;
+                String tomorrow;
+                if(day==1){
+                    yesterday = prefs.getString("yesterday","");
+                    tomorrow = codes[day+1];
+                }
+                else if(day==daysInMonth[month]){
+                    yesterday = codes[day-1];
+                    tomorrow = prefs.getString("tomorrow","");
+                }
+                else{
+                    yesterday = codes[day-1];
+                    tomorrow = codes[day+1];
+                }
+                otherCode.setText(yesterday+"\n"+tomorrow);
             }
             progressDialog.dismiss();
         }
